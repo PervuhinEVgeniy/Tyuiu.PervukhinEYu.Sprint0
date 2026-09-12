3fcd30be2189d856063bb9e10bee171d86c9cdd0
pervuhin.evgeniy7272@gmail.com <pervuhin.evgeniy7272@gmail.com>
2026-09-12 10:55:28 +0500

    Task2

---
+++ Tyuiu.PervuhinEY.Sprint0.Task2.V0.Tset/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PervuhinEY.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PervuhinEY.Sprint0.Task2.V0.Tset
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessegeValid()
+        {
+            //Области создания методов тестирования, методов из библиотеки 
+            var name = "Евгений";
+            var res = DataService.GetMessage(name);
+
+
+            //вызываем класс Assert и  метод AreEqual
+            Assert.AreEqual("Привет, Евгений", res);
+        }
+    }
+}
---
+++ Tyuiu.PervuhinEY.Sprint0.Task2.V0.Tset/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PervuhinEY.Sprint0.Task2.V0.Tset")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PervuhinEY.Sprint0.Task2.V0.Tset")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a8f4dd3-b0f9-45a7-8b41-a97d0bac9571")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
