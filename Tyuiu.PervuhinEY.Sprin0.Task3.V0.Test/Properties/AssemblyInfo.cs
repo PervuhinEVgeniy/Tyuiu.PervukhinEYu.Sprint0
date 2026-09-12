501d050fb5a227c785174bcfa9b64022caeb4a36
pervuhin.evgeniy7272@gmail.com <pervuhin.evgeniy7272@gmail.com>
2026-09-12 11:20:06 +0500

    Task3

---
+++ Tyuiu.PervuhinEY.Sprin0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PervuhinEY.Sprin0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PervuhinEY.Sprin0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90d2bdc0-8ab4-46b1-b69c-efd22350cb3a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PervuhinEY.Sprin0.Task3.V0Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PervuhinEY.Sprin0.Task3.V0Lib
+{
+    public class DataService
+    {
+        //Реализация метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
