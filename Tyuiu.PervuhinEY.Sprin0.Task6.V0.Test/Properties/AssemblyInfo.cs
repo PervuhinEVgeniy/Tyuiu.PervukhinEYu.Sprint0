753fe9188f77c15beb5e172abdda65b45b1b6366
pervuhin.evgeniy7272@gmail.com <pervuhin.evgeniy7272@gmail.com>
2026-09-13 22:49:35 +0500

    Task6

---
+++ Tyuiu.PervuhinEY.Sprin0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PervuhinEY.Sprin0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PervuhinEY.Sprin0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93f1d89b-5561-47cc-af02-9997aaf69707")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PervuhinEY.Sprin0.Task6.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PervuhinEY.Sprin0.Task6.V0.Lib;
+
+namespace Tyuiu.PervuhinEY.Sprin0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArrae(numsArray));
+
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
